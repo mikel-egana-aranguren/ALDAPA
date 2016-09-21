312625bf9f5e326b44c320d61193beb531cd76fd
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2016-09-21 15:53:10 +0200

    Added log4j2 support and RDF4J dependency

--- src/main/java/es/eurohelp/opendata/aldapa/Mikel.java
+++ src/main/java/es/eurohelp/opendata/aldapa/Mikel.java
@@ -3,11 +3,16 @@
  */
 package es.eurohelp.opendata.aldapa;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * @author megana
  *
  */
 public class Mikel {
+	
+	final static Logger logger = LogManager.getLogger(Mikel.class);
 
 	/**
 	 * Some development on branch dev
@@ -17,6 +22,9 @@ public class Mikel {
 	}
 	
 	public int evaluate(String expression) {
+		
+		logger.debug("This is debug");
+		
 	    int sum = 0;
 	    for (String summand: expression.split("\\+"))
 	      sum += Integer.valueOf(summand);
