d423b12de97355c704262373c4021c81c7349d5a
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2017-10-24 15:08:40 +0200

    Moved ManagerTest to core-plugins-tests
    Fixed SPARQLProtocolStoreTest

--- plugins/src/test/java/es/eurohelp/lod/aldapa/impl/test/SPARQLProtocolStoreTest.java
+++ plugins/src/test/java/es/eurohelp/lod/aldapa/impl/test/SPARQLProtocolStoreTest.java
@@ -27,16 +27,17 @@ import es.eurohelp.lod.aldapa.storage.RDFStoreException;
  */
 public class SPARQLProtocolStoreTest {
 
-	private static final String endpointUrl = "http://localhost:9999/blazegraph/sparql";
+	private static final String endpointUrl = "http://172.16.0.81:58080/blazegraph/namespace/aldapa/sparql";
 	private static final String graphQuery = "CONSTRUCT {?s ?p ?o} WHERE {?s ?p ?o}";
 	private static final String tupleQuery = "SELECT * WHERE {?s ?p ?o}";
-	private static final String booleanQuery = "ASK {<http://blazegraph.com/blazegraph> <http://blazegraph.com/implements> <http://blueprints.tinkerpop.com>}";
+	private static final String booleanQuery = "ASK {<http://blazegraph.com/blazegraph> <http://blazegraph.com/implements> <http://example.com/testing>}";
 	private static final String updateQuery = "INSERT DATA {<http://blazegraph.com/blazegraph> <http://blazegraph.com/implements> <http://example.com/testing>}";
-	private static final String booleanQuery2 = "ASK {<http://blazegraph.com/blazegraph> <http://blazegraph.com/implements> <http://example.com/testing>}";
-	private static final String stmt = "(http://blazegraph.com/blazegraph, http://blazegraph.com/implements, http://blueprints.tinkerpop.com)";
+	private static final String updateQuery2 = "INSERT DATA {<http://blazegraph.com/blazegraph> <http://blazegraph.com/implements> <http://example.com/testing2>}";
+	private static final String booleanQuery2 = "ASK {<http://blazegraph.com/blazegraph> <http://blazegraph.com/implements> <http://example.com/testing2>}";
+	private static final String stmt = "(http://blazegraph.com/blazegraph, http://blazegraph.com/implements, http://example.com/testing)";
 	private static final String subject = "http://blazegraph.com/blazegraph";
 	private static final String predicate = "http://blazegraph.com/implements";
-	private static final String object = "http://blueprints.tinkerpop.com";
+	private static final String object = "http://example.com/testing";
 	private static SPARQLProtocolStore sparqlStore;
 	
 	@BeforeClass
@@ -44,18 +45,14 @@ public class SPARQLProtocolStoreTest {
 		sparqlStore = new SPARQLProtocolStore(endpointUrl);
 	}
 	
-	@AfterClass
-	public static void tearDownAfterClass(){
-		sparqlStore.shutdownAtOnce();
-	}
-
 	@Test
-	public final void testSPARQLProtocolStoreString() {
+	public final void testSPARQLProtocolStore() {
 		assertNotNull(sparqlStore);
 	}
 
 	@Test
 	public final void testExecSPARQLGraphQuery() throws RDFStoreException {
+		sparqlStore.execSPARQLUpdate(updateQuery);
 		GraphQueryResult result = sparqlStore.execSPARQLGraphQuery(graphQuery);
 		boolean contains = false;
 		while(result.hasNext()){
@@ -69,6 +66,7 @@ public class SPARQLProtocolStoreTest {
 
 	@Test
 	public final void testExecSPARQLTupleQuery() {
+		sparqlStore.execSPARQLUpdate(updateQuery);
 		TupleQueryResult result = sparqlStore.execSPARQLTupleQuery(tupleQuery);
 		List<String> bindingNames = result.getBindingNames();
 		boolean contains = false;
@@ -86,12 +84,13 @@ public class SPARQLProtocolStoreTest {
 
 	@Test
 	public final void testExecSPARQLBooleanQuery() throws RDFStoreException {
+		sparqlStore.execSPARQLUpdate(updateQuery);
 		assertTrue(sparqlStore.execSPARQLBooleanQuery(booleanQuery));
 	}
 
 	@Test
 	public final void testExecSPARQLUpdate() throws RDFStoreException {
-//		sparqlStore.execSPARQLUpdate(updateQuery);
-//		System.out.println(sparqlStore.execSPARQLBooleanQuery(booleanQuery2));
+		sparqlStore.execSPARQLUpdate(updateQuery2);
+		assertTrue(sparqlStore.execSPARQLBooleanQuery(booleanQuery2));
 	}
 }
