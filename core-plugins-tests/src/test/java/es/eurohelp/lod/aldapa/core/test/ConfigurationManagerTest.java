e07c0ef01d77a69e8bf1f8a741d36dcc9e6f8d03
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2017-11-08 15:00:36 +0100

    [ci skip]

--- core-plugins-tests/src/test/java/es/eurohelp/lod/aldapa/core/test/ConfigurationManagerTest.java
+++ core-plugins-tests/src/test/java/es/eurohelp/lod/aldapa/core/test/ConfigurationManagerTest.java
@@ -5,12 +5,9 @@ package es.eurohelp.lod.aldapa.core.test;
 
 import static org.junit.Assert.*;
 
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import org.apache.http.client.ClientProtocolException;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.impl.TreeModel;
 import org.eclipse.rdf4j.model.util.ModelBuilder;
@@ -20,7 +17,6 @@ import org.eclipse.rdf4j.model.vocabulary.RDF;
 import org.junit.Test;
 
 import es.eurohelp.lod.aldapa.core.ConfigurationManager;
-import es.eurohelp.lod.aldapa.core.exception.AldapaException;
 import es.eurohelp.lod.aldapa.modification.FunctionalRDFQualityValidator;
 import es.eurohelp.lod.aldapa.storage.FunctionalFileStore;
 import es.eurohelp.lod.aldapa.storage.FunctionalRDFStore;
@@ -32,24 +28,24 @@ import es.eurohelp.lod.aldapa.transformation.FunctionalCSV2RDFBatchConverter;
  */
 public class ConfigurationManagerTest {
 	
-	private static final String ConfigFile = "configuration.yml";
-	private static final String configfile2 = "configuration2.yml";
-	private static final String ejiefile = "estaciones.csv";
-	private static final String ejiefileurl = "https://raw.githubusercontent.com/opendata-euskadi/LOD-datasets/master/calidad-aire-en-euskadi-2017/estaciones.csv";
-	private static final String csvfile = "data/OpenDataEuskadiCalidadDelAire/estaciones.csv";
+	private static final String CONFIGFILE = "configuration.yml";
+	private static final String CONFIGFILE2 = "configuration2.yml";
+	private static final String EJIEFILE = "estaciones.csv";
+	private static final String EJIEFILEURL = "https://raw.githubusercontent.com/opendata-euskadi/LOD-datasets/master/calidad-aire-en-euskadi-2017/estaciones.csv";
+	private static final String CSVFILE = "data/OpenDataEuskadiCalidadDelAire/estaciones.csv";
 	
 	@Test
 	public void testFileStore() throws Exception{
-		ConfigurationManager testManager = ConfigurationManager.getInstance(ConfigFile);
+		ConfigurationManager testManager = ConfigurationManager.getInstance(CONFIGFILE);
 		FunctionalFileStore fileStore = testManager.getFileStore();
-		fileStore.getFileHTTP(ejiefileurl, ejiefile, true);
+		fileStore.getFileHTTP(EJIEFILEURL, EJIEFILE, true);
 		assertEquals("data/",fileStore.getDirectoryPath());
-		assertTrue(Files.exists(Paths.get(fileStore.getDirectoryPath() + ejiefile)));
+		assertTrue(Files.exists(Paths.get(fileStore.getDirectoryPath() + EJIEFILE)));
 	}
 	
 	@Test
 	public void testMemoryRDFStore () throws Exception {
-		ConfigurationManager testManager = ConfigurationManager.getInstance(ConfigFile);
+		ConfigurationManager testManager = ConfigurationManager.getInstance(CONFIGFILE);
 		FunctionalRDFStore rdfStore = testManager.getRDFStore();
 		ModelBuilder builder = new ModelBuilder();
 		builder.setNamespace("ex", "http://example.org/").subject("ex:Picasso").add(RDF.TYPE, "ex:Artist").add(FOAF.FIRST_NAME, "Pablo");
@@ -59,30 +55,28 @@ public class ConfigurationManagerTest {
 	
 	@Test
 	public void testRESTRDFStore () throws Exception {
-		ConfigurationManager testManager = ConfigurationManager.getInstance(configfile2);
+		ConfigurationManager testManager = ConfigurationManager.getInstance(CONFIGFILE2);
 		FunctionalRDFStore rdfStore = testManager.getRDFStore();
 		ModelBuilder builder = new ModelBuilder();
 		builder.setNamespace("ex", "http://example.com/").subject("ex:Mikel").add(RDF.TYPE, "ex:Developer").add(FOAF.FIRST_NAME, "Mikel");
 		Model model = builder.build();
 		rdfStore.saveModel(model);
-//		FunctionalDBRDFStore dbRDFStore = (FunctionalDBRDFStore)rdfStore; 
-//		dbRDFStore.deleteDB(testManager.getConfigPropertyValue(tripleStoreConfigFile, dbNameToken));
 	}
 	
 	@Test
 	public void testTransformer () throws Exception {
-		ConfigurationManager testManager = ConfigurationManager.getInstance(ConfigFile);
+		ConfigurationManager testManager = ConfigurationManager.getInstance(CONFIGFILE);
 		FunctionalCSV2RDFBatchConverter converter = testManager.getTransformer();
 		Model model = new TreeModel();
-		converter.setDataSource(csvfile);
+		converter.setDataSource(CSVFILE);
 		converter.setModel(model);
 		Model new_model = converter.getTransformedModel("http://euskadi.eus/graph/calidad-aire");
 		assertNotNull(new_model);
 	}
 	
 	@Test
-	public void testValidator () throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException, AldapaException{
-		ConfigurationManager testManager = ConfigurationManager.getInstance(ConfigFile);
-		FunctionalRDFQualityValidator validator = testManager.getRDFQualityValidator();
+	public void testValidator () throws Exception{
+		ConfigurationManager testManager = ConfigurationManager.getInstance(CONFIGFILE);
+		assertNotNull(testManager.getRDFQualityValidator());
 	}
 }
