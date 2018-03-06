64cbe2d7a444360c80448b5b5122ee577ee92b05
Mishel Uchuari <dmuv7@hotmail.com>
2018-03-06 09:06:02 +0100

    rama grafter

---
+++ plugins/src/main/java/es/eurohelp/lod/aldapa/impl/transformation/GrafterRDFConverter.java
@@ -0,0 +1,72 @@
+package es.eurohelp.lod.aldapa.impl.transformation;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.Statement;
+
+import clojure.lang.LazySeq;
+import clojure.lang.RT;
+import es.eurohelp.lod.aldapa.core.exception.AldapaException;
+import es.eurohelp.lod.aldapa.transformation.CSV2RDFBatchConverter;
+import es.eurohelp.lod.aldapa.transformation.FunctionalCSV2RDFBatchConverter;
+import es.eurohelp.lod.aldapa.util.TripleAdder;
+
+public class GrafterRDFConverter extends CSV2RDFBatchConverter implements FunctionalCSV2RDFBatchConverter {
+
+	private String pipelinePath;
+	private String dataSource;
+	private String outputFilePath;
+	private String methodToExecute;
+	private Model model;
+
+	public GrafterRDFConverter() {
+		super();
+	}
+
+	@Override
+	public void setDataSource(String filePath) throws AldapaException {
+		this.dataSource = filePath;
+	}
+
+	public void setPipeline(String pipeline) {
+		this.pipelinePath = pipeline;
+	}
+
+	public void setOutputFile(String output) {
+		this.outputFilePath = output;
+	}
+
+	@Override
+	public void setModel(Model model) {
+		this.model = model;
+	}
+
+	public void setMainPipelineMethod(String methodToExecute) {
+		this.methodToExecute = methodToExecute;
+	}
+
+	@Override
+	public Model getTransformedModel(String namedGraphURI) {
+		TripleAdder adder = null;
+		try {
+			RT.loadResourceScript(pipelinePath);
+			pipelinePath = pipelinePath.replaceAll("/", ".");
+			pipelinePath = pipelinePath.replace(".clj", "");
+			LazySeq lazy = (LazySeq) RT.var(this.pipelinePath, this.methodToExecute).invoke(this.dataSource);
+			Iterator ite = lazy.iterator();
+			adder = new TripleAdder(this.model, namedGraphURI);
+			while (ite.hasNext()) {
+				Statement statement = (Statement) ite.next();
+				this.model.add(statement);
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return adder.getModel();
+	}
+
+}
