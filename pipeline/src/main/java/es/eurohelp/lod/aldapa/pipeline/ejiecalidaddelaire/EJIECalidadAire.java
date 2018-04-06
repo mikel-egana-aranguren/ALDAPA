4567e5e0f65bc8ef76d8f744d6842b4d214313e7
Mishel Uchuari <dmuv7@hotmail.com>
2018-04-06 14:37:15 +0200

    sonar

--- pipeline/src/main/java/es/eurohelp/lod/aldapa/pipeline/ejiecalidaddelaire/EJIECalidadAire.java
+++ pipeline/src/main/java/es/eurohelp/lod/aldapa/pipeline/ejiecalidaddelaire/EJIECalidadAire.java
@@ -17,45 +17,45 @@ import es.eurohelp.lod.aldapa.core.Manager;
  */
 public class EJIECalidadAire {
 
-	private EJIECalidadAire() {
-		throw new IllegalAccessError("Utility class");
-	}
+    private EJIECalidadAire() {
+        throw new IllegalAccessError("Utility class");
+    }
 
-	public static void main(String[] args) {
-		// Load the configuration from file configuration.yml
-		ConfigurationManager config = ConfigurationManager.getInstance("configuration.yml");
+    public static void main(String[] args) {
+        // Load the configuration from file configuration.yml
+        ConfigurationManager config = ConfigurationManager.getInstance("configuration.yml");
 
-		// Create a manager with the configuration
-		Manager manager = new Manager(config);
+        // Create a manager with the configuration
+        Manager manager = new Manager(config);
 
-		// Add project
-		String projectUri = manager.addProject("EuskadiMedioAmbiente");
+        // Add project
+        String projectUri = manager.addProject("EuskadiMedioAmbiente");
 
-		// Add catalog
-		String catalogUri = manager.addCatalog("CalidadAire", projectUri);
+        // Add catalog
+        String catalogUri = manager.addCatalog("CalidadAire", projectUri);
 
-		// Add dataset
-		String datasetUri = manager.addDataset("Estaciones", catalogUri);
+        // Add dataset
+        String datasetUri = manager.addDataset("Estaciones", catalogUri);
 
-		// Add namedGraph
-		String namedGraphUri = manager.addNamedGraph("Estaciones01", datasetUri);
+        // Add namedGraph
+        String namedGraphUri = manager.addNamedGraph("Estaciones01", datasetUri);
 
-		// Add data to named graph
-		manager.updateFileHTTP(
-				"https://raw.githubusercontent.com/opendata-euskadi/LOD-datasets/master/calidad-aire-en-euskadi-2017/estaciones.csv",
-				"estaciones.csv");
-		manager.addDataToNamedGraph(namedGraphUri, "estaciones.csv");
+        // Add data to named graph
+        manager.updateFileHTTP(
+                "https://raw.githubusercontent.com/opendata-euskadi/LOD-datasets/master/calidad-aire-en-euskadi-2017/estaciones.csv",
+                "estaciones.csv");
+        manager.addDataToNamedGraph(namedGraphUri, "estaciones.csv");
 
-		// Validate data
-		manager.analyseGraph();
+        // Validate data
+        manager.analyseGraph();
 
-		// Discover links
-		manager.discoverLinks();
-		
-		// Flush backbone
-		manager.flushGraph(null, "data/EuskadiMedioAmbienteMetadata.ttl", RDFFormat.TURTLE);
+        // Discover links
+        manager.discoverLinks();
 
-		// Flush data from named graph
-		manager.flushGraph(namedGraphUri, "data/EuskadiMedioAmbienteData.ttl", RDFFormat.TURTLE);
-	}
+        // Flush backbone
+        manager.flushGraph(null, "data/EuskadiMedioAmbienteMetadata.ttl", RDFFormat.TURTLE);
+
+        // Flush data from named graph
+        manager.flushGraph(namedGraphUri, "data/EuskadiMedioAmbienteData.ttl", RDFFormat.TURTLE);
+    }
 }
