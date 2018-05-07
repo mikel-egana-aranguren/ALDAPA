970c8d795baea378440d8512799f73e1cda51295
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2018-05-07 15:44:12 +0200

    Final GraphDB plugin

--- plugins/src/main/java/es/eurohelp/lod/aldapa/impl/storage/GraphDBStore.java
+++ plugins/src/main/java/es/eurohelp/lod/aldapa/impl/storage/GraphDBStore.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.Set;
 
-import org.apache.http.MethodNotSupportedException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.eclipse.rdf4j.model.Model;
--- plugins/src/test/java/es/eurohelp/lod/aldapa/impl/test/GraphDBStoreTest.java
+++ plugins/src/test/java/es/eurohelp/lod/aldapa/impl/test/GraphDBStoreTest.java
@@ -5,18 +5,16 @@ package es.eurohelp.lod.aldapa.impl.test;
 
 import static org.junit.Assert.*;
 
-import java.io.IOException;
-
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.util.ModelBuilder;
 import org.eclipse.rdf4j.model.vocabulary.FOAF;
 import org.eclipse.rdf4j.model.vocabulary.RDF;
-import org.junit.AfterClass;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import es.eurohelp.lod.aldapa.impl.storage.GraphDBStore;
-import es.eurohelp.lod.aldapa.storage.RDFStoreException;
 
 /**
  * @author megana
@@ -27,8 +25,10 @@ public class GraphDBStoreTest {
     static GraphDBStore graphdb = null;
     static final String graphdburl = "http://localhost:7200";
     static final String dbname = "ALDAPAGraphDBConnectionTests"; // This repository should already exist in GraphDB
-    static final String newdbname = "ALDAPAGraphDBConnectionTestsNewDB"; 
-    static final String systemdbname = "SYSTEM"; 
+    static final String deleteQuery = "DELETE DATA { <http://example.org/Picasso> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://example.org/Artist> . <http://example.org/Picasso> <http://xmlns.com/foaf/0.1/firstName> \"Pablo\" }";
+    static final String askQuery = "ASK WHERE { <http://example.org/Picasso> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://example.org/Artist> . <http://example.org/Picasso> <http://xmlns.com/foaf/0.1/firstName> \"Pablo\" }";
+    static final String tupleQuery = "SELECT * WHERE { ?s ?p ?o }";
+    static final String graphQuery = "CONSTRUCT { ?s ?p ?o }  WHERE { ?s ?p ?o }";
     
     
     @BeforeClass
@@ -40,35 +40,42 @@ public class GraphDBStoreTest {
     public final void testGraphDBStore() {
         assertNotNull(graphdb);
     }
-
-//    @Test
-//    public final void testGetDBs() {
-//        assertTrue((graphdb.getDBs().contains(systemdbname)));
-//        assertTrue((graphdb.getDBs().contains(dbname)));
-//    }
-//
-//    @Test
-//    public final void testCreateDB() {
-//        try {
-//            graphdb.createDB(newdbname);
-//        } catch (RDFStoreException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
     
+    @Before
+    public final  void saveModel(){
+        graphdb.saveModel(createModel());
+    }
+
     @Test
     public final void testSaveModel(){
+        assertTrue(graphdb.execSPARQLBooleanQuery(askQuery));
+    }
+    
+    @Test
+    public final void testRemoveModel(){
+        graphdb.execSPARQLUpdate(deleteQuery);
+        assertFalse(graphdb.execSPARQLBooleanQuery(askQuery));
+    }
+    
+    @Test
+    public final void testTupleQuery(){
+        assertEquals(3,graphdb.execSPARQLTupleQuery(tupleQuery).getBindingNames().size());
+    }
+    
+    @Test
+    public final void testGraphQuery(){
+        assertTrue(graphdb.execSPARQLGraphQuery(graphQuery).hasNext());
+
+    }
+    
+    @After
+    public final void removeModel() {
+        graphdb.execSPARQLUpdate(deleteQuery);
+    }
+    
+    private Model createModel (){
         ModelBuilder builder = new ModelBuilder();
         builder.setNamespace("ex", "http://example.org/").subject("ex:Picasso").add(RDF.TYPE, "ex:Artist").add(FOAF.FIRST_NAME, "Pablo");
-        Model model = builder.build();
-        graphdb.saveModel(model);
+        return builder.build();
     }
-
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception {
-        // Delete DB
-    }
-
 }
